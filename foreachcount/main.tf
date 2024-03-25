8ee32ac09521bd5d958b88d8f68bb496f924dd78
khadar155 <rayeezblaze1998@gmail.com>
2024-03-25 14:57:09 +0530

    up

---
+++ foreachcount/main.tf
@@ -0,0 +1,28 @@
+
+variable "ami" {
+  type    = string
+  default = "ami-0440d3b780d96b29d"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "sandboxes" {
+  type    = list(string)
+  default = ["testt", "prodt"]
+}
+
+  
+
+resource "aws_instance" "sandbox" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  for_each      = toset(var.sandboxes)
+
+#   count = length(var.sandboxes)
+  tags = {
+    Name = each.value # for a set, each.value and each.key is the same
+  }
+}
