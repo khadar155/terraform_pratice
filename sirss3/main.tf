2b2178b0f366f0464bcf7c60ac9ad95625d58c7e
khadar155 <rayeezblaze1998@gmail.com>
2024-03-26 18:13:54 +0530

    up

---
+++ sirss3/main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "example3e3e" {
+  bucket = var.mybucket
+  
+}
--- workspace/main.tf
+++ workspace/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  
+}
 resource "aws_s3_bucket" "ex" {
 
     bucket = "adfhadhflsdfhsdgadg"
